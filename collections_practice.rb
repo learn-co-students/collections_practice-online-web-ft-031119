1ca548d567943212e0f6c00f14b5ef3ac758f88f
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 22:21:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array_of_integers)
+  array_of_integers.sort
+end
 
+def sort_array_desc(array_of_integers)
+  array_of_integers.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array_of_strings)
+  array_of_strings.to_a.sort_by { |x| x.length }
+end
+
+def swap_elements(array, index, destination_index)
